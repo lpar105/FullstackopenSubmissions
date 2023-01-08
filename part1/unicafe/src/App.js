1a2a989c5ca2b563990e81b7023f7721f321df4d
Lauren <lpar105@aucklanduni.ac.nz>
2023-01-08 16:43:48 +1300

    1.11

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -25,7 +25,10 @@ const Statistics = ({good, neutral, bad}) => {
 
 const StatisticsLine = ({text, value}) => {
   return (
-    <p>{text}: {value}</p>
+    <tr>
+      <td>{text}:</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
