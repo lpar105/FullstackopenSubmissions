02cf0ecd54c3a69aad0eb211b9fa282b299846ec
Lauren <lpar105@aucklanduni.ac.nz>
2023-02-28 14:42:14 +1300

    2.12

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,61 +1,67 @@
-import axios from 'axios'
-import { useState, useEffect } from 'react'
-import { Filter } from './Components/FilterComponent'
-import { PersonForm } from './Components/PersonFormComponent'
-import { NumberList } from './Components/NumberListComponent'
-
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Filter } from "./Components/FilterComponent";
+import { PersonForm } from "./Components/PersonFormComponent";
+import { NumberList } from "./Components/NumberListComponent";
 
 const App = () => {
   // States
-  const [persons, setPersons] = useState([]) 
-  const [newName, setNewName] = useState('')
-  const [newNumber, setNewNumber] = useState('')
-  const [filter, setFilter] = useState('')
-  
+  const [persons, setPersons] = useState([]);
+  const [newNumber, setNewNumber] = useState("");
+  const [filter, setFilter] = useState("");
+
   // Effects
   useEffect(() => {
-    axios.get('http://localhost:3001/persons').then(response => { console.log(response.data); setPersons(response.data)})
-  }, [])
+    axios.get("http://localhost:3001/persons").then((response) => {
+      console.log(response.data);
+      setPersons(response.data);
+    });
+  }, []);
 
   // Event Handlers
   const handleNumberEntry = (event) => {
     setNewNumber(event.target.value);
-  }
+  };
 
   const handleFilterChange = (event) => {
     setFilter(event.target.value);
-  }
+  };
 
   const handleFormSubmit = (event) => {
-    event.preventDefault()
-    const name = event.target[0].value
+    event.preventDefault();
+    const name = event.target[0].value;
 
     if (getPerson(name)) {
-      return alert(`${name} is already added to the phonebook`)
+      return alert(`${name} is already added to the phonebook`);
     }
 
     addNumber(name);
-  }
+  };
 
   // Util
   const addNumber = (name) => {
-    setNewName(name);
-    setPersons(persons.concat({name, number:newNumber, id:persons.length+1}));
-  }
+    const newPerson = { name, number: newNumber, id: persons.length + 1 };
+    axios.post("http://localhost:3001/persons", newPerson);
+    setPersons(persons.concat(newPerson));
+  };
 
   const getPerson = (name) => {
-    return persons.find(e => e.name === name)
-  }
+    return persons.find((e) => e.name === name);
+  };
 
   return (
     <div>
       <h2>Phonebook</h2>
       <Filter handleFilterChange={handleFilterChange} />
-      <PersonForm handleFormSubmit={handleFormSubmit} newNumber={newNumber} handleNumberEntry={handleNumberEntry}/>
+      <PersonForm
+        handleFormSubmit={handleFormSubmit}
+        newNumber={newNumber}
+        handleNumberEntry={handleNumberEntry}
+      />
       <h2>Numbers</h2>
-      <NumberList persons={persons} filter={filter}/>
+      <NumberList persons={persons} filter={filter} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
